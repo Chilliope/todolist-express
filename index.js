3541f0554903879f55e8b329db95f799d98a646c
Erlang Andriyanputra <chilliope48@gmail.com>
2025-07-04 01:18:23 +0800

    get & create todolist

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const userRoutes = require('./routes/userRoutes');
 const todolistRoutes = require('./routes/todolistRoutes');
 
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
 app.use('/api/todolist', todolistRoutes)
 
 module.exports = app;
--- models/index.js
+++ models/index.js
@@ -1,10 +1,8 @@
 const sequelize = require('../config/database');
-const User = require('./user');
 const Todolist = require('./todolist');
 
 const db = {
   sequelize,
-  User,
   Todolist
 };
 
